47c5d375ac24204952329b50ef96016c3754e8c4
mahfuz ahmed <mahfuzkst2@gmail.com>
2024-05-25 14:23:10 +0600

    OOP

---
+++ OOP/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Person
+    {
+        public string firstName;   // Class Field/ Property / Atribute
+        public string lastName;  // When it contain data that time its called instance variable
+
+        public string FullName()
+        {
+            string fullName = firstName + " " + lastName ;
+            return fullName;
+        }
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.firstName = "Mahfuz";
+            person.lastName = "Ahmed";
+            Console.WriteLine(person.FullName());
+
+            Console.ReadLine();
+        }
+    }
+}
