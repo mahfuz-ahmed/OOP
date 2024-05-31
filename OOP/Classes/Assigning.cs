a13ffcb7e1735d258c8beb3a2b75c7ac83868c66
mahfuz ahmed <mahfuzkst2@gmail.com>
2024-05-31 13:19:21 +0600

    assigning

---
+++ OOP/Classes/Assigning.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Classes
+{
+    public class Assigning
+    {
+        string fullName = string.Empty;
+
+        public void Person()
+        {
+            Console.WriteLine("...Object Concept...");
+
+            Person person = new Person();                                                // person is a refference in the  Person Type
+            person.firstName = "Md.";
+            person.middleName = "Mahfuz";
+            //if(person.firstName.Length < 12)
+            //{
+            //    return;
+            //}
+            person.lastName = "Ahmed";
+            fullName = person.FullName();
+            Console.WriteLine("My Name is " + fullName);
+            Console.WriteLine();
+
+            //Refference Type / User Define Type
+            Console.WriteLine("...Refference Type / User Define Type / Non-primitive...");
+        }
+        public void Constructor()
+        {
+            Constructor constructor = new Constructor("Md.", "Ahmed", "mahfuz@gmial.com");
+            Constructor constructor1 = new Constructor("mahfuz@gmail.com");
+            fullName = constructor.MyDetails();
+            Console.WriteLine(fullName);
+
+            Constructor constructor2 = new Constructor();                               // Call Default Constructor
+            constructor2.FirstName = "Mahfuz";
+            constructor2.LastName = "Default Constractor";
+            fullName = constructor2.MyDetails();
+            Console.WriteLine(fullName);
+        }
+        public void ValueAndRefferenceType()
+        {
+            ValueType_RefferenceType refferenceType1 = new ValueType_RefferenceType();   // Create New instance or empty object in the memory
+            refferenceType1.firstName = "Abdul";                                         // Initializing value in the empty object
+            refferenceType1.middleName = "Rahman";
+            refferenceType1.lastName = "Shohag";
+            fullName = refferenceType1.FullName();
+            Console.WriteLine(fullName);
+            Console.WriteLine();
+
+            ValueType_RefferenceType refferenceType2 = new ValueType_RefferenceType();   // Create New instance or empty object in the memory
+            refferenceType2.firstName = "Md.";                                           // Initializing value in the empty object
+            refferenceType2.middleName = "Mohidul";
+            refferenceType2.lastName = "Islam";
+            fullName = refferenceType2.FullName();
+            Console.WriteLine(fullName);
+            Console.WriteLine();
+
+            ValueType_RefferenceType refferenceType3 = refferenceType1;                 // This refference indicate to refferenceType1 Object
+
+            refferenceType3.lastName = "Changed";                                       // LastName changed in the refferenceType1 object
+
+            fullName = refferenceType1.FullName();                                      // Here Changed the value in the refferenceType1 Object
+            Console.WriteLine(fullName);
+            fullName = refferenceType3.FullName();                                      // Those refference indicate to the same object thats whay, if change any refferences changed to both
+            Console.WriteLine(fullName);
+            Console.WriteLine();
+
+            //Value Type 
+            Console.WriteLine("...Value Type / Premitive Type...");
+            int firstNumber = 200;
+            int secondNumber = firstNumber;                                             // Create a new memory location for second number, so if change to firsNumber not empact to secondNumber
+            firstNumber = 500;
+
+            Console.WriteLine("Second Number = " + secondNumber);
+            Console.WriteLine("After Change firstNumber = " + firstNumber);
+        }
+        public void Encapsulation()
+        {
+            //Encapsulation / Property
+            Console.WriteLine("...Encapsulation...");
+            Encapsulation encapsulation = new Encapsulation("mahfuz", "ahmed");          // Calling by constructor
+            //encapsulation.FirstName = "Mahfuz";
+            encapsulation.FatherName = "Md.Nobis Uddin";
+            string motherName = encapsulation.MotherName;                               // You can not set motherName here, just you can get motherName
+            Console.WriteLine("My Name: " + encapsulation.FirstName);
+            Console.WriteLine("Father Name: " + encapsulation.FatherName);
+        }
+
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -11,81 +11,11 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            string fullName;
-            // Object
-            Console.WriteLine("...Object Concept...");
-
-            Person person = new Person();                                                // person is a refference in the  Person Type
-            person.firstName = "Md.";
-            person.middleName = "Mahfuz";
-            person.lastName = "Ahmed";
-            fullName = person.FullName();
-            Console.WriteLine("My Name is "+fullName);
-            Console.WriteLine();
-
-            //Refference Type / User Define Type
-            Console.WriteLine("...Refference Type / User Define Type / Non-primitive...");
-
-            ValueType_RefferenceType refferenceType1 = new ValueType_RefferenceType();   // Create New instance or empty object in the memory
-            refferenceType1.firstName = "Abdul";                                         // Initializing value in the empty object
-            refferenceType1.middleName = "Rahman";
-            refferenceType1.lastName = "Shohag";
-            fullName = refferenceType1.FullName();
-            Console.WriteLine(fullName);
-            Console.WriteLine();
-
-            ValueType_RefferenceType refferenceType2 = new ValueType_RefferenceType();   // Create New instance or empty object in the memory
-            refferenceType2.firstName = "Md.";                                           // Initializing value in the empty object
-            refferenceType2.middleName = "Mohidul";
-            refferenceType2.lastName = "Islam";
-            fullName = refferenceType2.FullName();
-            Console.WriteLine(fullName);
-            Console.WriteLine();
-
-            ValueType_RefferenceType refferenceType3 = refferenceType1;                 // This refference indicate to refferenceType1 Object
-
-            refferenceType3.lastName = "Changed";                                       // LastName changed in the refferenceType1 object
-
-            fullName = refferenceType1.FullName();                                      // Here Changed the value in the refferenceType1 Object
-            Console.WriteLine(fullName);
-            fullName = refferenceType3.FullName();                                      // Those refference indicate to the same object thats whay, if change any refferences changed to both
-            Console.WriteLine(fullName);
-            Console.WriteLine();
-
-            //Value Type 
-            Console.WriteLine("...Value Type / Premitive Type...");
-            int firstNumber = 200;
-            int secondNumber = firstNumber;                                             // Create a new memory location for second number, so if change to firsNumber not empact to secondNumber
-            firstNumber = 500;
-
-            Console.WriteLine("Second Number = "+secondNumber);
-            Console.WriteLine("After Change firstNumber = "+firstNumber);
-
-
-            //Encapsulation / Property
-            Console.WriteLine("...Encapsulation...");
-            Encapsulation encapsulation = new Encapsulation("mahfuz","ahmed");          // Calling by constructor
-            //encapsulation.FirstName = "Mahfuz";
-            encapsulation.FatherName = "Md.Nobis Uddin";
-            string motherName = encapsulation.MotherName;                               // You can not set motherName here, just you can get motherName
-            Console.WriteLine("My Name: "+ encapsulation.FirstName);
-            Console.WriteLine("Father Name: "+encapsulation.FatherName);
-
-
-            Constructor constructor = new Constructor("Md.","Ahmed","mahfuz@gmial.com");
-            Constructor constructor1 = new Constructor("mahfuz@gmail.com");
-            fullName = constructor.MyDetails();
-            Console.WriteLine(fullName);
-
-            Constructor constructor2 = new Constructor();                               // Call Default Constructor
-            constructor2.FirstName = "Mahfuz";
-            constructor2.LastName = "Default Constractor";
-            fullName = constructor2.MyDetails();
-            Console.WriteLine(fullName);
-
-
-
-
+            Assigning assign = new Assigning();
+            assign.Person();
+            assign.Constructor();
+            assign.ValueAndRefferenceType();
+            assign.Encapsulation();
             Console.ReadLine(); 
         }
     }
